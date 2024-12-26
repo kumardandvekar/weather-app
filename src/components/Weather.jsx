abe2d832daed5ac8160be720810cf0dc3521f3a6
Kumar Dandvekar <94390889+kumardandvekar@users.noreply.github.com>
2024-12-26 14:51:35 +0530

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -39,10 +39,6 @@ const Weather = () => {
       const response = await fetch(url);
       const data = await response.json();
 
-      
-
-
-
       console.log(data); 
 
       const icon = allIcons[data.weather[0].icon] || clear_icon;
